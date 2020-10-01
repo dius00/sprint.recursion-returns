c760c5789afd08ab4b139731c2095ca7f0d06cef
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-01 10:26:52 +0900

    renamed function to have a more descriptive name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,13 +1,13 @@
 const getElementsByClassName = (target) => {
   const resultArr = [];
-  recursive(document.body, target);
-  function recursive(element, target) {
+  classFinder(document.body, target);
+  function classFinder(element, target) {
     if (element.className.includes(target)) {
       resultArr.push(element);
     }
     if (element.children) {
       for (const child of element.children) {
-        recursive(child, target);
+        classFinder(child, target);
       }
     }
   }
